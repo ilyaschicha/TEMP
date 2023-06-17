a12f6cb03dce8b1dcf73a0f4db45eddd9d8c3e92
Chicha Ilyes <chicha.ilyes.business@gmail.com>
2023-06-17 01:17:11 +0100

    adding Administrateur Model

--- app/Models/Administrateur.php
+++ app/Models/Administrateur.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Administrateur extends Model
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class Administrateur extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = "administrateur";
+    protected $fillable = [
+        'nom_admin',
+        'prenom_admin',
+        'tel_admin',
+        'email_admin',
+        'password',
+        'ville',
+    ];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
