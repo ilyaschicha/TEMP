ebbb137f9cb65ef9ac123b65f2e675a28ffad595
Chicha Ilyes <chicha.ilyes.business@gmail.com>
2023-06-17 01:17:43 +0100

    adding Dentiste Model

--- app/Models/Dentiste.php
+++ app/Models/Dentiste.php
@@ -1,22 +1,42 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Dentiste extends Model
+class Dentiste extends  Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = "dentiste";
+    protected $table = "dentistes";
     protected $fillable = [
-        'NomDent',
-        'PrenomDent',
-        'TelDent',
-        'EmailDent',
-        'Password',
-        'Ville',
-        'Adresse',
+        'nom_dent',
+        'prenom_dent',
+        'tel_dent',
+        'email_dent',
+        'password',
+        'ville',
+        'adresse',
+        'roles',
+    ];
+        /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
